9e33a5afabca69d8dc43f1f4547178a85c4ef9e4
rosenmeier <morr@itu.dk>
2014-03-25 12:31:57 +0100

    database tables created

--- PublishITService/PublishITDatabase.sql
+++ PublishITService/PublishITDatabase.sql
@@ -2,7 +2,7 @@
 
 CREATE TABLE [user] (
 	[user_id] INT(10) NOT NULL IDENTITY(1,1) PRIMARY KEY,
-	[role_id] INT(10) NOT NULL DEFAULT(1) FOREIGN KEY REFERENCES role(role_id),
+	[role_id] INT(10) NOT NULL DEFAULT(1) FOREIGN KEY REFERENCES [role]([role_id]),
 	[organization_id] INT(10) NOT NULL FOREIGN KEY REFERENCES organization(organization_id),
 	name VARCHAR(45) NOT NULL,
 	email VARCHAR(45) NOT NULL,
@@ -29,6 +29,7 @@ CREATE TABLE [media] (
 /* Video table */
 
 CREATE TABLE video (
+	video_id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
 	media_id INT(10) NOT NULL FOREIGN KEY REFERENCES media(media_id),
 	[length] FLOAT,
 	number_of_rents INT(5),
@@ -45,15 +46,66 @@ CREATE TABLE document (
 /* Genre*/
 
 CREATE TABLE genre (
-	genre_id int(10) NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	genre_id INT(2) NOT NULL IDENTITY(1,1) PRIMARY KEY,
 	genre VARCHAR(20)
 );
 
  /* MediaGenre*/
 
 CREATE TABLE media_genre (
-	media_id INT(2) NOT NULL FOrEIGN KEY REFERENCES [media](media_id),
-	genre_id INT(2) NOT NULL FOREIGN KEY REFERENCES [genre](genre_id),
+	media_id INT(2) NOT NULL FOREIGN KEY REFERENCES [media](media_id),
+	genre_id INT(2) NOT NULL FOREIGN KEY REFERENCES [genre](genre_id)
 );
 
 
+CREATE TABLE [format] (
+	format_id INT(2) NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	media_format VARCHAR(20)
+);
+
+
+CREATE TABLE person (
+	person_id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	name VARCHAR(100) NOT NULL,
+	birthday VARCHAR(10)
+);
+
+
+CREATE TABLE profession (
+	profession_id INT(3) NOT NULL PRIMARY KEY,
+	title VARCHAR(50)
+);
+
+
+CREATE TABLE person_in_video (
+	video_id INT NOT NULL FOREIGN KEY REFERENCES video(video_id),
+	person_id INT NOT NULL FOREIGN KEY REFERENCES person(person_id),
+	profession_id INT(3) NOT NULL FOREIGN KEY REFERENCES profession(professioon_id)
+);
+
+
+CREATE TABLE rent (
+	video_id INT NOT NULL FOREIGN KEY REFERENCES video(video_id),
+	[user_id] INT NOT NULL FOREIGN KEY REFERENCES [user]([user_id]),
+	[start_date] DATETIME NOT NULL,
+	[end_date] DATETIME NOT NULL,
+);
+
+
+CREATE TABLE rating (
+	[user_id] INT NOT NULL FOREIGN KEY REFERENCES [user]([user_id]),
+	[media] INT NOT NULL FOREIGN KEY REFERENCES [media]([media_id]),
+	rating INT(2) NOT NULL
+);
+
+
+CREATE TABLE [role] (
+	[role_id] INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	[role] VARCHAR(50)
+);
+
+
+CREATE TABLE organization (
+	organization_id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	organizatin VARCHAR(50)
+);
